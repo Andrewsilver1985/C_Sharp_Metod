14c897eff60713a2d790a09a8b0767a446e96d5d
Andrew <AndrewSilver@mail.ru>
2024-03-28 21:41:48 +0300

    Первая задача про двуерные мвссивы

---
+++ DvumerMassiv/Task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int[,] Creat(int rowCount, int colCount)
+{
+    int[,] table = new int[rowCount, colCount];
+    Random rnd = new Random();
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(1, 11);
+
+
+        }
+    }
+    return table;
+}
+
+void ShowTable(int[,] table)
+{
+    Random rnd=new Random();
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(1, 11);
+            System.Console.Write($"{table[i, j]} ");
+
+
+        }
+        System.Console.WriteLine();
+    }
+}
+ShowTable(Creat(4,6));
