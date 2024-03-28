d012be32af176c90882617c486bc5fddf5f94ff1
Andrew <AndrewSilver@mail.ru>
2024-03-28 10:59:03 +0300

    Добавили третью задачу

--- Lection_Function/Task_2/Program.cs
+++ Lection_Function/Task_2/Program.cs
@@ -1,26 +1,10 @@
-﻿System.Console.WriteLine("Привет");
-System.Console.WriteLine("Введите число а");
-int a = int.Parse(Console.ReadLine());
-
-System.Console.WriteLine("Введите число b");
-int b = int.Parse(Console.ReadLine());
-
-System.Console.WriteLine("Введите число c");
-int c = int.Parse(Console.ReadLine());
-
-System.Console.WriteLine("Введите число d");
-int d = int.Parse(Console.ReadLine());
-
-double Rezult(int a, int b, int c, int d)
+﻿void Sq(int N)
 {
-    double rez = (a * b) ;
-    int rez1= (c + d);
-    double itog = rez/rez1;
-   return itog;
-    //System.Console.WriteLine(itog);
-
+    for (int i = 1; i < N; i++)
+    {
+        int sq = i * i;
+        System.Console.Write($" {sq} ");
+    }
 }
-System.Console.WriteLine(Rezult(a,b,c,d));
 
-
-
+Sq(9);
---
+++ Lection_Function/Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] arr = { 1, 2, 3, 4, 5, 6 };
+void Zero(int[] arr)
+{
+    for (int i = 0; i < 6; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+
+        }
+        System.Console.WriteLine(arr[i]);
+    }
+
+}
+Zero(arr);
