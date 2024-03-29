b1710b53035e26fb5eafb596840d49616ec6819e
Andrew <AndrewSilver@mail.ru>
2024-03-29 09:47:53 +0300

    Решили задачу с интересными числами

---
+++ DvumerMassiv/Task_2/Program.cs
@@ -0,0 +1,49 @@
+﻿int[,] Creat(int rowCount, int colCount)
+{
+    int[,] table = new int[rowCount, colCount];
+    Random rnd = new Random();
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(1, 1000);
+
+
+        }
+    }
+    return table;
+
+}
+
+bool Interes(int value)
+{
+    int sumDig = GetSumOfDig(value);
+
+    if (sumDig % 2 == 0)
+        return true;
+    else
+        return false;
+
+
+}
+int GetSumOfDig(int value)
+{
+    int sum = 0;
+    while (value > 0)
+    {
+        sum += value % 10;
+        value /= 10;
+
+    }
+    return sum;
+}
+
+int[,] arr = Creat(3, 4);
+    foreach (var item in arr)
+    {
+        if (Interes(item) == true)
+        {
+            System.Console.WriteLine($"{item} ");
+        }
+    }
+
