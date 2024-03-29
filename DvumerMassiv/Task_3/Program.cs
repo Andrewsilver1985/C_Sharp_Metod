94ddd62b6303276d5b129e5fdb61bcb329d6c473
Andrew <AndrewSilver@mail.ru>
2024-03-29 11:39:38 +0300

    Первая задача со троками

---
+++ DvumerMassiv/Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿string GetLeter(string s)
+{
+    string letter = "";
+    foreach (var e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letter = letter + e;
+        }
+
+    }
+    return letter;
+
+
+}
+string str = Console.ReadLine();
+string result = GetLeter(str);
+
+System.Console.WriteLine(result);
